8aaa5488f516826a48526dba387f782f775f32fe
salamella4335 <102366123+salamella4335@users.noreply.github.com>
2026-03-25 02:44:25 +0100

    Add files via upload

---
+++ src/constants/difficultyColors.js
@@ -0,0 +1,3 @@
+export const difficultyColors = [
+    "green", "orange", "red", "black"
+]
---
+++ src/constants/markerTypeColors.js
@@ -0,0 +1,9 @@
+export const markerTypeColors = {
+    "bikeTrail": "#0e7424",
+    "hikeTrail": "#724218",
+    "eBikeStation": "#005f8b",
+    "boatStation": "#156c72",
+    "trainStation": "#8f1414",
+    "boatRoute": "#258ee4",
+    "default": "#2d54d3"
+}
